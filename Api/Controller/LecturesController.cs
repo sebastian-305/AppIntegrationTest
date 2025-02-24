b46a0929cdefa042e0649435c23ab40f883e14b6
Sebastian Bogensperger <106682134+sebastian-305@users.noreply.github.com>
2025-02-24 16:21:13 +0100

    controller finished

--- Api/Controller/LecturesController.cs
+++ Api/Controller/LecturesController.cs
@@ -18,6 +18,7 @@ public class LecturesController : ControllerBase
         _repository = repository;
     }
 
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Lecture>>> GetAll()
     {
@@ -27,33 +28,57 @@ public class LecturesController : ControllerBase
     }
 
 
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpGet("{id}")]
-    public ActionResult<Lecture> Get(int id)
+    public async Task<ActionResult<Lecture>> GetAsync(int id)
     {
-        if (id != 1)
-        {
+        var lecture = await _repository.FindByIdAsync(id);
+
+        if (lecture == null)
             return NotFound();
-        }
 
-        return Ok(new Lecture() { Id = 1 });
+        return Ok(lecture);
     }
 
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    
     [HttpPost]
-    public ActionResult<Lecture> Post(Lecture lecture)
+    public async Task<ActionResult<Lecture>> Post(Lecture lecture)
     {
-        return Ok(new Lecture() { });
+        var newLecture = await _repository.AddAsync(lecture);
+        return Created(nameof(GetAsync),newLecture);
     }
 
-
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpPut("{id}")]
-    public ActionResult<Lecture> Put(int id)
+    public async Task<ActionResult<Lecture>> PutAsync(int id, Lecture putLecture)
     {
-        return Ok(new Lecture() { });
+        var lecture = await _repository.FindByIdAsync(id);
+
+        if (lecture == null)
+            return NotFound();
+
+        putLecture.Id = id;
+
+        await _repository.UpdateAsync(putLecture);
+
+        return Ok(putLecture);
     }
 
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpDelete("{id}")]
-    public ActionResult<Lecture> Delete(int id)
+    public async Task<ActionResult<Lecture>> DeleteAsync(int id)
     {
-        return Ok(new Lecture() { });
+        var lecture = await _repository.FindByIdAsync(id);
+
+        if (lecture == null)
+            return NotFound();
+
+       await _repository.DeleteAsync(lecture);
+
+        return Ok(lecture);
     }
 }
